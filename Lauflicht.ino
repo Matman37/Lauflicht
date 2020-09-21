49caed47314d9342ddf2a5aa9451a31e0177b61c
Matman37 <71316447+Matman37@users.noreply.github.com>
2020-09-21 19:01:17 +0200

    Add files via upload

--- Lauflicht.ino
+++ Lauflicht.ino
@@ -1,3 +1,5 @@
+
+byte LED [4] = {13,12,11,10};
 void setup() {
 
 pinMode(13, OUTPUT);
@@ -5,33 +7,12 @@ pinMode(12, OUTPUT);
 pinMode(11, OUTPUT);
 pinMode(10, OUTPUT);
 
-#define roteLED1 13
-#define roteLED2 12
-#define roteLED3 11
-#define roteLED4 10
-
 }
 
 void loop() {
-                           // Lauflicht rechts         
-digitalWrite(roteLED1, HIGH);
+
+    for(byte i = 0; i<4; i++)
+digitalWrite(LED, HIGH);
 delay(180);
-digitalWrite(roteLED1, LOW);
-digitalWrite(roteLED2, HIGH);
-delay(180);
-digitalWrite(roteLED2, LOW);
-digitalWrite(roteLED3, HIGH);
-delay(180);
-digitalWrite(roteLED3, LOW);
-digitalWrite(roteLED4, HIGH);
-delay(180);
-digitalWrite(roteLED4, LOW);
-                            //Lauflicht links
-digitalWrite(roteLED3, HIGH);
-delay(180);
-digitalWrite(roteLED3, LOW);
-digitalWrite(roteLED2, HIGH);
-delay(180);
-digitalWrite(roteLED2, LOW);
 
 }
